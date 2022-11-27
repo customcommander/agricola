42e67232fb3186b960a6b37e78699e141a333a60
customcommander <hello@spinjs.com>
2022-11-27 10:22:52 +0000

    consolidate setup service & state

--- src/xstate/main-machine.js
+++ src/xstate/main-machine.js
@@ -1,24 +1,21 @@
-import {createMachine} from 'xstate';
+import {createMachine, forwardTo} from 'xstate';
 
 export default () => createMachine({
   context: {},
   initial: 'init',
   states: {
     init: {
-      on: {
-        SETUP_GAME: {
-          target: 'setup'
-        }
-      }
-    },
-    setup: {
       invoke: {
+        id: 'setup-service',
         src: () => new Promise(res => {
           // Simulates async operations whilst setting up a game (e.g. dynamic imports)
           setTimeout(() => res(true), 50);
         }),
         onDone: {
           target: 'turn_1'
+        },
+        on: {
+          SETUP_GAME: forwardTo('setup-service')
         }
       }
     },
