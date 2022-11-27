0600c933a52ec517aa3d5c3ae87d8062ea07b2db
customcommander <hello@spinjs.com>
2022-11-27 19:57:10 +0000

    colocate main machine and interpreter

--- src/xstate/main.js
+++ src/xstate/main.js
@@ -1,5 +1,58 @@
-import {interpret} from 'xstate';
-import machine from './main-machine.js';
+import {interpret, createMachine, forwardTo} from 'xstate';
+
+import * as actions from './actions/index.js';
+import * as guards from './guards/index.js';
+
+const machine_def = {
+  context: {
+    turn: 0,
+    stage: 0,
+  },
+  initial: 'init',
+  states: {
+    init: {
+      invoke: {
+        id: 'setup-service',
+        src: () => new Promise(res => {
+          // Simulates async operations whilst setting up a game (e.g. dynamic imports)
+          setTimeout(() => res(true), 50);
+        }),
+        onDone: {
+          target: 'work'
+        },
+      },
+      on: {
+        SETUP_GAME: forwardTo('setup-service')
+      }
+    },
+    work: {
+      entry: 'new_turn',
+      on: {
+        NEW_TURN: {
+          target: 'work'
+        },
+        HARVEST_TIME: {
+          target: 'feed'
+        }
+      }
+    },
+    feed: {
+      invoke: {
+        id: 'feed-service',
+        src: () => new Promise(res => {
+          setTimeout(() => res(true), 50);
+        }),
+        onDone: [
+          {target: 'work', cond: 'not_end_of_game'},
+          {target: 'end_of_game'}
+        ]
+      }
+    },
+    end_of_game: {
+      type: 'final'
+    }
+  }
+};
 
 // TODO:
 // Eventually this event will be responsible for:
@@ -12,7 +65,7 @@ const initGame = () => ({
 
 // Starts a new game unless `events` is not nil (i.e replaying a game)
 export default (events) => {
-  const game = interpret(machine());
+  const game = interpret(createMachine(machine_def, {actions, guards}));
   game.start();
   game.send(events ?? [initGame()]);
   return game;
