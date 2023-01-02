b2ee1183fcd7cd55d4c27569862ff15ab17091b4
customcommander <hello@spinjs.com>
2023-01-02 10:28:36 +0000

    move cucumber.js to test directory

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# this script is executed via `npm test` from the project root directory,
+# so paths are relative from the root.
 
-npx cucumber-js test/features
+npx cucumber-js --config test/cucumber.js test/features
