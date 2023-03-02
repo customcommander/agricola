5e0c757d191690f919abfa01c83a8405d7b2f548
customcommander <hello@spinjs.com>
2023-03-02 08:19:56 +0000

    move setup to invoked callback

--- src/engine/core.cljs
+++ src/engine/core.cljs
@@ -1,5 +1,6 @@
 (ns engine.core
   (:require [xstate]
+            [engine.services.setup :as setup]
             [engine.services.work :as work]))
 
 (def machine-opts
@@ -7,29 +8,49 @@
        #js {:forward-to-work
             (xstate/forwardTo work/id)
 
+            :forward-to-setup
+            (xstate/forwardTo "setup-service")
+
             :bye
             (fn []
-              (println "game over! bye ;)"))}
+              (println "game over! thank you for playing ;)"))}
        
        :services
-       #js {:work
+       #js {:setup
+            setup/get-service
+
+            :work
             work/get-service}})
 
 (defn get-machine []
   (xstate/createMachine
    #js {:context
         #js {:num-workers 2}
-        :initial "work"
+        :initial "setup"
         :states
-        #js {:work
+        #js {:setup
+             #js {:invoke
+                  #js {:id "setup-service"
+                       :src "setup"}
+
+                  :on
+                  #js {:setup-game
+                       #js {:actions "forward-to-setup"}
+
+                       :setup-done
+                       #js {:target "work"}}}
+
+             :work
              #js {:invoke
                   #js {:id work/id
                        :src "work"
                        :onDone
                        #js {:target "end"}}
+
                   :on
                   #js {:task-selected
                        #js {:actions "forward-to-work"}
+
                        :task-done
                        #js {:actions "forward-to-work"}}}
              :end
@@ -37,16 +58,16 @@
                   :type "final"}}}
    machine-opts))
 
-
 (defn main []
   (let [game (xstate/interpret (get-machine))]
     [game #(.send (.start game) %)]))
 
 (comment
-  (let [[_ start] (main)]
-    (start #js [#js {:type "task-selected"}
-                #js {:type "task-done"}
-                #js {:type "task-selected"}
-                #js {:type "task-done"}]))
-)
+  (let [[g start] (main)]
+    (start #js {:type "setup-game"})
+    (.send g #js {:type "task-selected"})
+    (.send g #js {:type "task-done"})
+    (.send g #js {:type "task-selected"})
+    (.send g #js {:type "task-done"}))
+  )
 
--- src/engine/services/setup.cljs
+++ src/engine/services/setup.cljs
@@ -1,9 +1,10 @@
 (ns engine.services.setup
-  (:require [xstate]))
+  (:require [xstate]
+            [goog.functions :as fun]))
 
-
-
-
-
-
+(defn get-service []
+  (fn [respond listen]
+    (listen (fn []
+              (respond #js {:type "setup-done"})))
+    fun/EMPTY))
 
