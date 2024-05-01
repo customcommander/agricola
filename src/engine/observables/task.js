3cac6c6c6f4795b72416c223ea12fe574261416d
Julien Gonzalez <julien.gonzalez@signal-ai.com>
2024-05-01 18:11:04 +0100

    remove old files that i dont need anymore

--- src/engine/observables/task.js
+++
@@ -1,7 +0,0 @@
-import { concatMap, distinct, from } from "rxjs";
-
-export default (game$) =>
-  game$.pipe(
-    concatMap(st => from(Object.values(st.context.tasks))),
-    distinct(task => task.change_id)
-  );
