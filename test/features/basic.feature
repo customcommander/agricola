4aa5b92ea75e5e05f80c9d2008361290f30ba13a
customcommander <hello@spinjs.com>
2024-06-08 08:16:17 +0100

    delete old tests as deprecated

--- test/features/basic.feature
+++
@@ -1,6 +0,0 @@
-Feature: Basic functionalities
-
-Scenario: Sequence
-  Given I start a new game
-  When I complete 16 rounds
-  Then the game ends
--- test/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# this script is executed via `npm test` from the project root directory,
-# so paths are relative from the root.
-
-npx cucumber-js --config test/cucumber.js test/features
-npx cucumber-js --config test/cucumber.js --profile predefined_setup test/features
