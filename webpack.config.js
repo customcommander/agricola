034be4f05a78cf6b3a1339516db2d080def573d5
customcommander <hello@spinjs.com>
2024-09-05 20:21:53 +0100

    export webpack config as a function

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-export default {
-  entry: './src/component-app.js',
-  mode: 'development',
-  output: {
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: [/\bmessages_en\.yaml$/],
-        type: 'javascript/auto',
-        loader: '@messageformat/loader',
-        options: { locale: ['en'] }
-      }
+export default function webpack_config(env, argv) {
+  return {
+    entry: './src/component-app.js',
+    mode: 'development',
+    output: {
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\bmessages_en\.yaml$/],
+          type: 'javascript/auto',
+          loader: '@messageformat/loader',
+          options: { locale: ['en'] }
+        }
+      ]
+    },
+    devServer: {
+      static: './dist'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Agricola',
+        scriptLoading: 'module'
+      })
     ]
-  },
-  devServer: {
-    static: './dist'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Agricola',
-      scriptLoading: 'module'
-    })
-  ]
-};
+  };
+}
 
