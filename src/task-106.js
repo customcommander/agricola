c92a890a7d595895f6c4bed3d716f8f5a959bffc
customcommander <hello@spinjs.com>
2024-10-10 07:57:10 +0100

    migrate task 106 to use lib-task

--- src/task-106.js
+++ src/task-106.js
@@ -2,26 +2,13 @@
   Day Laborer
 */
 
-import {base} from './task-lib.js';
+import task from './lib-task.js';
 
-function collect(_, game_context) {
-  game_context.supply.food += 2;
-  return game_context;
-}
-
-export default base.createMachine({
-  initial: 'idle',
-  states: {
-    idle: {
-      on: {
-        'task.selected': {
-          actions: [
-            {type: 'game-update', params: {updater: collect}},
-            {type: 'task-complete'}
-          ]
-        }
-      }
-    }
+export default task({
+  id: '106',
+  execute: (_, game) => {
+    game.supply.food += 2;
+    return game;
   }
 });
 
